b11542ec412ce54bb2c482e6e0c6a447245008fd
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2023-02-27 20:37:41 +0200

    add pge resourse posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
 
     public function index(): Factory|View|Application
-    {
+    { 
         return view('home');
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,20 +7,11 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class UserController extends Controller
 {
     public function show($show = 'index'): Factory|View|Application
     {
         return view("user.$show");
     }
-//
-//    public function singup(): Factory|View|Application
-//    {
-//        return view('user.singup');
-//    }
-//
-//    public function index(): Factory|View|Application
-//    {
-//        return view('user.index');
-//    }
+
 }
